46cca32588dda4babd5dadfcc4eba36b38a5808b
duc2652001 <pmduc19@apcs.vn>
2021-05-28 21:10:58 +0800

    Add Assessment Screen
    Create ActionButton and ShowAssessmentScreen

--- src/screens/ShowAssessmentScreen.js
+++ src/screens/ShowAssessmentScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { 
+import {
   Image, 
   ImageBackground, 
   SafeAreaView, 
@@ -7,9 +7,10 @@ import {
   StatusBar, 
   StyleSheet, 
   Text, 
-  TouchableOpacity, 
+  TouchableOpacity,
   View 
 } from 'react-native'
+import ActionButton from "../values/actionButtons"
 
 export default ({ navigation }) => {
   const handleAvatarPress = () => {
@@ -24,67 +25,83 @@ export default ({ navigation }) => {
       <SafeAreaView style={styles.screen}>
         <ScrollView>
           <View style={styles.top}>
-            <TouchableOpacity onPress={handleAvatarPress}>
-              <Image 
-                style={styles.avatar}
-                source={require('../../assets/images/avatar-sample.png')}
-                resizeMode='contain'
-              />
-            </TouchableOpacity>
+            <ActionButton />
             <Text style={styles.header}>Assessment</Text>
           </View>
           <View></View>
           <View style={styles.container}>
-            <Text style={styles.text}>
-              Sorry, this feature is currently unavailable :(.
-            </Text>
+              <TouchableOpacity>
+                     <Text style={styles.title}>Upcoming Events</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={styles.text}> MA1521 Final Exam {"\n"} 21-Nov-2020 2:00 PM 4MC</Text>
+              </TouchableOpacity>
           </View>
+          <View style={styles.container}>
+                 <TouchableOpacity>
+                     <Text style={styles.title}>Current Modules</Text>
+                 </TouchableOpacity>
+                 <Text style={styles.text}>Empty</Text>
+          </View>
+
         </ScrollView>
       </SafeAreaView>
     </ImageBackground>
   )
 }
 
+
 const styles = StyleSheet.create({
   background: {
-    flex: 1, 
-  }, 
+    flex: 1,
+  },
   screen: {
-    flex: 1, 
-    paddingTop: StatusBar.currentHeight, 
-    margin: 20, 
-  }, 
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    margin: 20,
+  },
   top: {
-    flex: 0.08, 
-    flexDirection: 'row', 
-    justifyContent: 'flex-start', 
-    alignItems: 'flex-start', 
-  }, 
+    flex: 0.08,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
   container: {
-    flex: 0.92, 
-    alignItems: 'center', 
-    paddingHorizontal: 40, 
-    paddingTop: 20, 
-  }, 
+    flex: 0.92,
+    alignItems: 'flex-start',
+    marginLeft: 50,
+    paddingTop: 20,
+    height: 300,
+  },
   avatar: {
-    marginRight: 20, 
-    width: 50, 
-    height: 50, 
-  }, 
+    marginRight: 20,
+    width: 50,
+    height: 50,
+  },
   header: {
-    color: '#ffffff', 
-    fontSize: 24, 
-    fontWeight: 'bold', 
-    lineHeight: 24, 
-    textAlign: 'center', 
-    alignSelf: 'center',  
-  }, 
+    color: '#ffffff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    lineHeight: 24,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  title: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    lineHeight: 24,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
   text: {
-    color: '#ffffff', 
-    fontSize: 16, 
-    fontWeight: 'normal', 
-    lineHeight: 20, 
-    textAlign: 'center', 
-    alignSelf: 'center', 
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'normal',
+    lineHeight: 20,
+    alignSelf: 'flex-start',
+    borderRadius: 5,
+    borderWidth: 1,
+    backgroundColor: '#8e8a98',
   }, 
 })
---
+++ src/values/actionButtons.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Animated,
+  Image,
+  ImageBackground,
+  Text,
+  TouchableWithoutFeedback,
+  Alert,
+} from 'react-native';
+
+export default class ActionButton extends Component {
+  state = {
+    animation: new Animated.Value(0),
+  };
+  toggleOpen = () => {
+
+    if (this._open) {
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: 1000,
+      }).start();
+
+    } else {
+
+      Animated.timing(this.state.animation, {
+        toValue: 1,
+        duration: 1000,
+      }).start();
+    }
+    this._open = !this._open
+
+  };
+  render() {
+    const printInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 50],
+    });
+    const saveInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 50, 100],
+    });
+
+    const printStyle = {
+      transform: [
+        {
+          translateY: printInterpolate,
+        },
+      ],
+    };
+
+    const saveStyle = {
+      transform: [
+        {
+          translateY: saveInterpolate,
+        },
+      ],
+    };
+    return (
+      <View style = {styles.container}>
+        <Animated.View style = {styles.background}>
+          <TouchableWithoutFeedback>
+            <Animated.View style={[styles.button, saveStyle]}>
+                <Image
+                    style={{height:25, width:25}}
+                    source={require("../../assets/home.jpg")}
+                />
+            </Animated.View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback>
+            <Animated.View style={[styles.button, printStyle]}>
+                <Image
+                    style={{height:25, width:25}}
+                    source={require("../../assets/logout.jpg")}
+                />
+            </Animated.View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => this.toggleOpen()}>
+            <Animated.View style={styles.button}>
+              <Image
+                style={{height:50, width:50}}
+                resizeMode="contain"
+                source={require('../../assets/images/avatar-sample.png')}
+              />
+            </Animated.View>
+          </TouchableWithoutFeedback>
+        </Animated.View>
+       </View>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex : 0.2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 25,
+        marginBottom: 30,
+        marginRight: 30,
+        marginLeft: 10,
+    },
+    mainButton:{
+        position: 'absolute',
+        alignItems: 'center',
+        alignSelf: 'center',
+        width: 50,
+        height: 50,
+    },
+    logOut:{
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+  button: {
+    marginTop: 5,
+    position: 'absolute',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  background: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  }
+
+})
